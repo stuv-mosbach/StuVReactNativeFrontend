6cd6230f670223759537e802302f384e3f0dba6d
the_loraxxz <the_loraxxz@web.de>
2022-04-07 22:33:49 +0200

    Tab navigator --> startup extern muss noch gebaut werden

--- App.tsx
+++ App.tsx
@@ -1,18 +1,83 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Home from './src/pages/Home';
 import Startup from './src/pages/Startup';
+import Setting from './src/pages/Settings';
+import {Text, TouchableOpacity, View} from "react-native";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+
+function TabBarCustomized({state,descriptors,navigation}) {
+    console.log(state)
+    console.log(descriptors)
+    console.log(navigation)
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+                const label =
+                    options.tabBarLabel !== undefined
+                        ? options.tabBarLabel
+                        : options.title !== undefined
+                            ? options.title
+                            : route.name;
+
+                const isFocused = state.index === index;
+
+                const onPress = () => {
+                    const event = navigation.emit({
+                        type: 'tabPress',
+                        target: route.key,
+                        canPreventDefault: true,
+                    });
+
+                    if (!isFocused && !event.defaultPrevented) {
+                        // The `merge: true` option makes sure that the params inside the tab screen are preserved
+                        navigation.navigate({ name: route.name, merge: true });
+                    }
+                };
+
+                const onLongPress = () => {
+                    navigation.emit({
+                        type: 'tabLongPress',
+                        target: route.key,
+                    });
+                };
+
+                return (
+                    <TouchableOpacity
+                        accessibilityRole="button"
+                        accessibilityState={isFocused ? { selected: true } : {}}
+                        accessibilityLabel={options.tabBarAccessibilityLabel}
+                        testID={options.tabBarTestID}
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        style={{ flex: 1 }}
+                    >
+                        <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+                            {label}
+                        </Text>
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Startup" component={Startup} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home" tabBar={(props)=><TabBarCustomized {...props}/>}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Setting} />
+        <Tab.Screen name="Startup" component={Startup} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {View} from "react-native";
+
+export default function Settings({navigation}:any) {
+    return(
+        <View>
+
+        </View>
+    )
+}
