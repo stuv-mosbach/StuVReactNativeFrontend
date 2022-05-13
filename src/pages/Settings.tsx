b14ac65e54ddce282a8e80edd8f16b59464a3fd6
The Loraxxz <tilmann.lorenz@mail.com>
2022-05-13 18:43:12 +0200

    te

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SettingsOverview from "../components/SettingsOverview";
-import DevList from "./DevList";
-
-
+import SettingsOverview from '../components/SettingsOverview';
+import DevList from '../components/DevList';
 
 const Stack = createNativeStackNavigator();
 
-export default function Settings({navigation}: any) {
-    return (
-            <Stack.Navigator initialRouteName="Overview"
-                             screenOptions={{
-                                 presentation:"card",
-                             }}>
-                <Stack.Screen component={SettingsOverview}
-                              name={"Overview"}
-                              options={{ headerShown: false }}/>
-                <Stack.Screen name={"Devs"}
-                              component={DevList}
-                              options={{ headerShown: false }}/>
-            </Stack.Navigator>
-    )
+export default function Settings({ navigation }: any) {
+  return (
+    <Stack.Navigator
+      initialRouteName="Overview"
+      screenOptions={{
+        presentation: 'card',
+      }}>
+      <Stack.Screen
+        component={SettingsOverview}
+        name={'Overview'}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name={'Devs'}
+        component={DevList}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
 }
