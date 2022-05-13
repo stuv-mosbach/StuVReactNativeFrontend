eafc9de42db1b216ae4e365f3252ca726d2dbf91
The Loraxxz <tilmann.lorenz@mail.com>
2022-05-13 18:43:03 +0200

    moved dev list to right folder and formated everything

--- src/components/SettingsOverview.tsx
+++ src/components/SettingsOverview.tsx
@@ -1,7 +1,7 @@
-import {style} from "../util/Style";
-import {Text, View} from "react-native";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
-import React from "react";
+import { style } from '../util/Style';
+import { Text, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
 
 /**
  * TODO:
@@ -14,27 +14,26 @@ import React from "react";
  * -
  * */
 
-export default function SettingsOverview({navigation}:any) {
-    return(
-        <View style={style.settingsView}>
-            <Text>Kurs auswählen</Text>
-            <View style={style.divider}/>
-            <Text style={style.settingsHeader}>Version</Text>
-            <Text>2.0</Text>
-            <View style={style.settingsField} onTouchStart={()=>{
-                navigation.navigate("Devs")
-            }}>
-                <Text style={style.settingsFieldEntryLeft}>Devs</Text>
-                <FontAwesome
-                    name={"arrow-right"}
-                    style={style.settingsIcon}
-                />
-            </View>
+export default function SettingsOverview({ navigation }: any) {
+  return (
+    <View style={style.settingsView}>
+      <Text>Kurs auswählen</Text>
+      <View style={style.divider} />
+      <Text style={style.settingsHeader}>Version</Text>
+      <Text>2.0</Text>
+      <View
+        style={style.settingsField}
+        onTouchStart={() => {
+          navigation.navigate('Devs');
+        }}>
+        <Text style={style.settingsFieldEntryLeft}>Devs</Text>
+        <FontAwesome name={'arrow-right'} style={style.settingsIcon} />
+      </View>
 
-            <View style={style.divider}/>
-            <Text>Bugs melden</Text>
-            <View style={style.divider}/>
-            <Text>Stuv Teilnehmenö</Text>
-        </View>
-    )
+      <View style={style.divider} />
+      <Text>Bugs melden</Text>
+      <View style={style.divider} />
+      <Text>Stuv Teilnehmenö</Text>
+    </View>
+  );
 }
