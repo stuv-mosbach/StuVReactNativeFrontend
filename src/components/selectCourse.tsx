6ef8a2cbdb43d3b2f0ed17cb5404f5c0d2a12aa6
the_loraxxz <the_loraxxz@web.de>
2022-10-03 16:12:57 +0200

    starting fixing saving bug

--- src/components/selectCourse.tsx
+++ src/components/selectCourse.tsx
@@ -48,6 +48,7 @@ export function CourseSelecter() {
                     }
                 }
             );
+
         } else {
             Toast.show("Bitte mindestens einen Kurs auswählen!")
         }
